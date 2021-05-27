72b6ee136a623441930caf77758d7b48873e09d3
Kamran <iskama98@mail.ru>
2021-05-27 23:43:48 +0300

    feat: complete unit-2

--- unit2/unit_02.js
+++ unit2/unit_02.js
@@ -5,9 +5,12 @@
 // добавьте в него свойство семейство (family) равное например AMD Ryzen 7
 // добавьте в него название (name) например Ryzen 7 3700X
 
-const cpu = {
-
-}
+//const cpu = {
+//   'producer' : 'AMD',
+//   'amount' : '250.00',
+//   'family' : 'Vishera',
+//   'name' : 'FX-8320',
+//}
 
 //Task 2
 // Создайте наследника объекта cpu и назовите его memory (через proto).Да, мы будем строить магазин, который торгует и памятью.Поскольку мы наследуемся от cpu то все свойства останутся.
@@ -19,15 +22,58 @@ const cpu = {
 // family = DDR4
 
 // создаем memory тут.
+let memory = {
+   'memory' : '2048',
+   __proto__ : 'cpu'
+};
+ memory.producer = 'Hynix';
+ memory.amount = '60';
+ memory.memory = '2048';
+ memory.name = 'Hynix DDR4-2666';
+ memory.family = 'DDR4';
 
+ console.log(memory)
 
 //Task 3.
 // Представим себе ситуацию, что мы начали использовать данные объекты на странице и поняли что нам не хватает изображения. Добавим свойство image в объект cpu. Поскольку, memory наследуется cpu то данное свойство появится и у него. Просто дописываем данное свойство в cpu.
+
+//const cpu = {
+//   'producer' : 'AMD',
+//   'amount' : '250.00',
+//   'family' : 'Vishera',
+//   'name' : 'FX-8320',
+//   'image' : '',
+//}
+
 
 // Task 4. 
 // Добавьте в свойство image для cpu ссылку
 // https://i2.rozetka.ua/goods/1865699/copy_amd_fx_series_fx_9590_fd9590fhhkwof_58abf05e14fdc_58abfc46c2b65_58abff06c94bd_images_1865699744.jpg
 // а для memory - ссылку https://i2.rozetka.ua/goods/17090881/189441988_images_17090881613.jpg
 
+//const cpu = {
+//   'producer' : 'AMD',
+//   'amount' : '250.00',
+//   'family' : 'Vishera',
+//   'name' : 'FX-8320',
+//   'image' : 'https://i2.rozetka.ua/goods/1865699/copy_amd_fx_series_fx_9590_fd9590fhhkwof_58abf05e14fdc_58abfc46c2b65_58abff06c94bd_images_1865699744.jpg'
+//}
+
+//memory.image = 'https://i2.rozetka.ua/goods/17090881/189441988_images_17090881613.jpg';
+
 // Task 5. 
 // Добавьте для cpu метод sale(p) - который возвращает стоимость amount товара с учетом скидки p. Скидка задается в процентах. Например 5%.  
+
+const cpu = {
+   'producer' : 'AMD',
+   'amount' : '250.0',
+   'family' : 'Vishera',
+   'name' : 'FX-8320',
+   'image' : 'https://i2.rozetka.ua/goods/1865699/copy_amd_fx_series_fx_9590_fd9590fhhkwof_58abf05e14fdc_58abfc46c2b65_58abff06c94bd_images_1865699744.jpg',
+   
+   'sale' : function sale(p) {
+      return this.amount * (1 - p / 100)
+   }
+}
+
+console.log(cpu.sale(5));
