e9cd98d28a278bd84cae3f554fca3ae0d5de7bc1
Kamran <iskama98@mail.ru>
2021-05-30 15:12:36 +0300

    refactor: fix code

--- unit3/class/ModernButton.js
+++ unit3/class/ModernButton.js
@@ -1,11 +1,11 @@
 class ModernButton extends Button {
    constructor(width, height, background, value, borderRadius) {
-      super(width, height, background, value, borderRadius);
+      super(width, height, background, value);
       this.borderRadius = borderRadius;
    }
    render() {
-      element = super.render();
-      element = this.borderRadius + 'px';
+      let element = super.render();
+      element.style.borderRadius = this.borderRadius + 'px';
       return element
    }
 }
