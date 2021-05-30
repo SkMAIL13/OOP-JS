debe6e1deb3ae5d0982269d7ea11c3d043a39d77
Kamran <iskama98@mail.ru>
2021-05-30 14:45:16 +0300

    feat: new constructor

--- unit3/class/Button.js
+++ unit3/class/Button.js
@@ -1,7 +1,17 @@
 class Button {
-
+    constructor(width, height, background, value) {
+        this.width = width,
+        this.height = height,
+        this.background = background,
+        this.value = value
+    }
 
     render() {
-
+        let element = document.createElement('button');
+        element.style.width = this.width + 'px';
+        element.style.height = this.height + 'px';
+        element.style.background = this.background;
+        element.innerText = this.value;
+        return element;
     }
 }
