ce7746f5dc7cd39184eee42adf5fbb1af3b84ece
Kamran <iskama98@mail.ru>
2021-05-30 15:12:57 +0300

    refactor: fix render info

--- unit3/script.js
+++ unit3/script.js
@@ -2,10 +2,11 @@ let width = 200;
 let height = 100;
 let background = 'space-gray';
 let value = 'Unit-3';
-let borderRadius = 100;
+let borderRadius = 30;
 
 let button = new Button(width, height, background, value);
 document.body.append(button.render());
 
-let moderButton = new ModernButton(width, height, background, value, borderRadius);
-document.body.append(button.render());
+let modernButton = new ModernButton(width, height, background, value, borderRadius);
+document.body.append(modernButton.render());
+
