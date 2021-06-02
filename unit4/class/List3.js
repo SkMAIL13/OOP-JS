0ca08293ad6024d631b532a8304ee4e474efa93b
Kamran <iskama98@mail.ru>
2021-06-02 22:43:41 +0300

    feat: list3 extends list

--- unit4/class/List3.js
+++ unit4/class/List3.js
@@ -1,7 +1,13 @@
-class List {
-
-
+class List3 extends List {
+    constructor(item, cssClass) {
+        super(item);
+        this.cssClass = cssClass
+    }
     render() {
-
+        let ul = super.render();
+        for(let i = 0; i < this.cssClass; i++) {
+            ul.classList.add(tihs.cssClass[i])
+        }
+        return ul
     }
 }
