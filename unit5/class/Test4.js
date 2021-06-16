c9d2526ad9ad5bb60dd9f5954b743de5bdee48a3
Kamran <iskama98@mail.ru>
2021-06-16 17:01:37 +0300

    feat: add set/get to class4

--- unit5/class/Test4.js
+++ unit5/class/Test4.js
@@ -1,3 +1,17 @@
 class Test4 extends Test2 {
-
+   set testProp(a) {
+      this._testProp = a * 10;
+   }
+   set testValue(b) {
+      this._testValue = b;
+   }
+   settestValue(b) {
+      if (b > 100) {
+         this._testValue = b;
+         return b
+      } else {return false}
+   }
+   get testValue() {
+      return this._testValue
+   }
 }
