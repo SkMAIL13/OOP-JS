ee7f34b44460e9f3018c410a757e50fd91758937
Kamran <iskama98@mail.ru>
2021-06-16 17:02:43 +0300

    feat: add 'out'

--- unit5/script.js
+++ unit5/script.js
@@ -21,3 +21,14 @@
 //console.log(test3.testProp = 'zzz');
 //console.log(test3);
 //console.log(test3.testProp);
+
+//Task-5
+const test4 = new Test4();
+//console.log(test4.testProp = 'zzz');
+//console.log(test4);
+//console.log(test4.testProp);
+
+//console.log(test4.testValue = 11);
+//console.log(test4);
+console.log(test4.settestValue(11));
+console.log(test4);
