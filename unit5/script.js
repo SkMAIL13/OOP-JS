36b6f642a68f08caa65b4853b135f34d3dbdfc57
Kamran <iskama98@mail.ru>
2021-06-16 16:26:57 +0300

    feat: done ex-2

--- unit5/script.js
+++ unit5/script.js
@@ -4,4 +4,12 @@
 //console.log(out);
 //console.log(out.testProp);
 
+//Task-2
+//const test2 = new Test2();
+//console.log(test2.testProp = 'bbb'); 
+//console.log(test2);
+//console.log(test2.testProp = ''); 
+//console.log(test2);
+//console.log(test2.setTestProp(''));
+//console.log(test2);
 
