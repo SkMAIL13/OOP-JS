903c8db1f5197fea5813f1ef968e54ebc7d80fed
Kamran <iskama98@mail.ru>
2021-07-13 19:52:24 +0300

    feat: done ex8

--- unit8/script.js
+++ unit8/script.js
@@ -36,7 +36,9 @@ function f2() {
 }
 
 // f2.call();
+f2.call(document.querySelector('.div-2-1'))
 // f2.call()
+f2.call(document.querySelector('.div-2-2'))
 
 // ==================================================
 
@@ -48,8 +50,9 @@ function f3(color) {
 }
 
 // f3.call();
+f3.call(document.querySelector('.div-3-1'), 'red')
 // f3.call()
-
+f3.call(document.querySelector('.div-3-2'), 'orangered')
 // ==================================================
 
 
@@ -61,7 +64,9 @@ function f4(color) {
 }
 
 // f4.apply();
+f4.apply(document.querySelector('.div-4-1'),['red'])
 // f4.apply()
+f4.apply(document.querySelector('.div-4-2'), ['orange'])
 
 // ==================================================
 
@@ -73,7 +78,9 @@ function f5() {
 }
 
 // f5.apply
+f5.apply(document.querySelector('.div-5-1'))
 // f5.apply
+f5.apply(document.querySelector('.div-5-2'), [f5])
 
 // ==================================================
 
@@ -84,11 +91,11 @@ function f6() {
     this.style.background = 'orange'
 }
 
-// let bindF61 = f6.bind()
-// let bindF62 = f6.bind()
+ let bindF61 = f6.bind(document.querySelector('.div-6-1'));
+ let bindF62 = f6.bind(document.querySelector('.div-6-2'));
 
-// bindF61();
-// bindF62();
+ bindF61();
+ bindF62();
 
 // ==================================================
 
@@ -99,10 +106,10 @@ function f7(color) {
     this.style.background = color;
 }
 
-// let bindF71 = f7.bind()
-// let bindF72 = f7.bind()
+ let bindF71 = f7.bind(document.querySelector('.div-7-1'));
+ let bindF72 = f7.bind(document.querySelector('.div-7-2'));
 
-// bindF71();
-// bindF72();
+ bindF71('orange');
+ bindF72('blue');
 
 // ==================================================
